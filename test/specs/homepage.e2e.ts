9041941a3329750541de476c0feda1c2cf67a7ea
Nidhi Dutta <nidhi2627@gmail.com>
2023-04-23 19:50:58 +0000

    adding test for home page buy buttons

--- test/specs/homepage.e2e.ts
+++ test/specs/homepage.e2e.ts
@@ -1,9 +1,10 @@
 describe('Home page functionality tests', () => {
 
-    it('verify if Header tooltip icon is existing & popup appears on click', async() => {
+    beforeEach(async() => {
         await browser.url('https://weathershopper.pythonanywhere.com/')
-        await browser.maximizeWindow()
+    })
 
+    it('verify if Header tooltip icon is existing & popup appears on click', async() => {
         var toolTipIcon = await $('*[data-original-title=\'Your task\']')
         expect(toolTipIcon).toBeExisting()
         await toolTipIcon.click()
@@ -11,6 +12,21 @@ describe('Home page functionality tests', () => {
         var popup = await $('div.popover-body')
         expect(popup).toBeDisplayed()
         expect(await popup.getText()).toContain('Shop for moisturizers if the weather is below 19 degrees. Shop for suncreens if the weather is above 34 degrees.')
+    })
+
+    it('verify the navigation of Buy sunscreens & Buy moisturizers buttons', async() => {
+        var temperatureElem = await (await $('span#temperature')).getText()
+
+        var temperatureValue = temperatureElem.split(" ")[0]
+        
+        if(parseInt(temperatureValue) < 19) {
+            await (await $('=Buy moisturizers')).click()
+            expect(await browser.getUrl()).toContain('/moisturizer')
+        }
+        else if(parseInt(temperatureValue) > 34) {
+            await (await $('=Buy sunscreens')).click()
+            expect(await browser.getUrl()).toContain('/sunscreen')
+        }
     })
 
 })
